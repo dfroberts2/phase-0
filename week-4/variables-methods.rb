bf32b901d7f1d5a039fc660b09e3e36a1917bb25
David Roberts <DFRoberts2@gmail.com>
2015-09-30 21:43:17 -0400

    add reflection to variable-methods.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -9,3 +9,24 @@
 	favorite_num = gets.chomp
 	new_num = favorite_num.to_i + 1
 	puts 'Oh really? Well ' + new_num.to_s + ' is a bigger and better number.'
+
+	#How do you define a local variable?
+	#A local variable is definied with the "=" sign
+
+	#How do you define a method?
+	#A method is defined with "def"
+
+	#What is the difference between a local variable and a method?
+	#A local variable has a single value that could be a string, integer or float (among others). A method is a block of instructions in ruby that could have many inputted arguements
+
+	#How do you run a ruby program from the command line?
+	#By entering "ruby" and then typing the ruby file name
+
+	#How do you run an RSpec file from the command line?
+	#By entering "RSpec" and then the RSpec file
+
+	#What was confusing about this material? What made sense?
+	#Searching the code for syntax errors
+
+
+

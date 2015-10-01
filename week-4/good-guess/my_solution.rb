4d98a5fb8f74198ff78c4e47372d98f70fa44c6c
David Roberts <DFRoberts2@gmail.com>
2015-10-01 15:51:48 -0400

    create good guess method 4.5.1

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+def good_guess?(integer)
+	if integer == 42
+		return true
+	elsif integer != 42
+		return false
+	end
+end
+

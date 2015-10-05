790e08c611fe599ad595396f83b20b0ba2f16c13
David Roberts <DFRoberts2@gmail.com>
2015-10-04 21:32:05 -0400

    finish 4.7

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
@@ -13,26 +13,24 @@
 # Output: sum
 # Steps to solve the problem.
 
-# while the ith element of the array is not nil
-#	sum = the ith element of the array
-#	i = i + 1
-#	sum = the ith element of the array + sum
+# for each element in array a do the following:
+# add the element to a variable "sum"
+# return the sum value
 
-
-# 1. total initial solution
-def total(a)
-	sum = 0
-	i=0
-	while a[i] != nil
-		sum=a[i]+sum
-		i=i+1
-	end
-	return sum
-end
-
+#def total(a)
+#	sum = 0
+#	a.each do |x|
+#		sum = x + sum
+#	end
+#	return sum
+#end
 
 
 # 3. total refactored solution
+
+def total(a)
+	a.reduce(:+)
+end
 
 
 
@@ -41,11 +39,33 @@ end
 # Input: array of strings
 # Output: single string
 # Steps to solve the problem.
+# for each element in array a
+# add the element to a variable "string" + space
+# get rid of last extra space
+# add period
+
+
 
 
 # 5. sentence_maker initial solution
+#def sentence_maker(a)
+#	string = ""
+#	a[0] = a[0].capitalize
+#	a.each do |x|
+#		x =  x.to_s
+#		string = string + x + " "
+#	end
+#	string = string.chomp(" ")
+#	string = string + "."
+#	return string
+#end
 
 
 
 # 6. sentence_maker refactored solution
+def sentence_maker(a)
+	a[0] = a[0].capitalize
+	string = a.join(" ")+"."
+	return string
+end
 

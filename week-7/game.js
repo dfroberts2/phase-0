bbf52ebe07fd605c26cd744ff315383be2147919
David Roberts <DFRoberts2@gmail.com>
2015-10-23 13:31:01 -0400

    start game and write stories for group project

---
+++ week-7/game.js
@@ -0,0 +1,41 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission: Match all Cards
+// Goals: Make a match
+// Characters: n/a
+// Objects: 52 Cards, 1 Score Counter, HTML Canvas element
+// Functions: Card Flip, Score Add
+
+// Pseudocode
+// Create 52 Objects with properties:
+//	name (ex: QH), flip status, image
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
